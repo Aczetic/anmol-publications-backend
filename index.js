407849615946eefa709af856f28f04b4e18ba6e0
Ananya <justdiscussanything@gmail.com>
2025-09-09 15:28:53 +0530

    test commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import express from "express";
+import { configDotenv } from "dotenv";
 
+const app = express();
+configDotenv();
+
+app.get('/' , (req,res)=>{
+    res.send("yup it's working");
+})
+
+app.listen(process.env.PORT||3000 , ()=>console.log(`Listening at port ${process.env.PORT}`));
